1fc6364eb0895673a8fc0d891ca617045c95f835
DidierDefrance <didier@d-defrance.fr>
2024-09-09 16:02:46 +0200

    s

--- tp/appliJava/demo/src/main/java/tp/demo/rest/DeviseRestController.java
+++ tp/appliJava/demo/src/main/java/tp/demo/rest/DeviseRestController.java
@@ -1,7 +1,13 @@
 package tp.demo.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tp.demo.model.Devise;
+import tp.demo.service.ServiceDevise;
+
+import java.util.List;
 
 @RestController //Composant Spring de type controller de WS REST
 @RequestMapping(value = "/devise" , headers="Accept=application/json")
@@ -10,9 +16,10 @@ public class DeviseRestController {
     @Autowired
     private ServiceDevise serviceDevise;
 
+    //url de test : http://localhost:8181/demo/devise
     @GetMapping("")
-    public Devise getAllDevises(){
-        return serviceDevise....;
+    public List<Devise> getAllDevises(){
+        return serviceDevise.findAllDevises();
     }
 
 
