0b78fa243fc4468bc51072f6eb2688da0e1a48fa
DidierDefrance <didier@d-defrance.fr>
2024-09-09 15:06:46 +0200

    avec 8181 coté dev/java

--- tp/appliJava/demo/src/main/java/tp/demo/DemoApplication.java
+++ tp/appliJava/demo/src/main/java/tp/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("http://localhost:8282/demo");
+		System.out.println("http://localhost:8181/demo");
 	}
 
 }
