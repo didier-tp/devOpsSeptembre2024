16b62eeca26ef089e974dd12f33acc3e023cabd7
DidierDefrance <didier@d-defrance.fr>
2024-09-11 20:47:42 +0200

    s

--- tp/appliJava/demo/src/main/java/tp/demo/DemoApplication.java
+++ tp/appliJava/demo/src/main/java/tp/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		//System.setProperty("spring.profiles.default" , "reinit,mysql");
         System.setProperty("spring.profiles.default" , "reinit,h2"); //profils activés par défaut au démarrage de l'appli
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("http://localhost:8181/demo");
