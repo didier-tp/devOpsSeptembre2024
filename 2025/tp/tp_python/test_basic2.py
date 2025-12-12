3a05951bf0c671f42d255bf6756f4bb3b2ea52e4
Didier_Defrance <didier@d-defrance.fr>
2025-12-12 10:18:36 +0100

    s

--- 2025/tp/tp_python/test_basic2.py
+++ 2025/tp/tp_python/test_basic2.py
@@ -1,8 +1,12 @@
 from basic2 import multiplication
 
 def test_multiplication():
-    resMult=multiplication(3,5)
-    print("multiplication(3,5)=" + str(resMult))
+    x=3
+    y=5
+    resMult=multiplication(x,y)
+    #print("multiplication(3,5)=" + str(resMult))
+    #print("multiplication(3,5)=" , resMult)
+    print(f'multiplication avec x={x} et y={y} le res vaut {resMult}')
     assert resMult == 15
    
 # nb: lancement du test via la commande:
